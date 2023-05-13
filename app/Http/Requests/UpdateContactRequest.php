34215091d9e02fb61eeeef13a3d0e5d058c3bdbb
sam4work <sam4work10@gmail.com>
2023-05-13 11:12:24 +0000

    conatct crud endpoints created

---
+++ app/Http/Requests/UpdateContactRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateContactRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+            "first_name" => ["required", "max:50","alphanum"],
+            "last_name" => ["required", "max:50","alphanum"],
+        ];
+    }
+}
